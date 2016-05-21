27058e8f30f3ce66c07b1014e00299c8f475f1e8
Evgeniy Pelmenev <g.snake345@gmail.com>
2016-05-21 18:20:20 +0200

    test input

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 var myNews = [
 	{
 		author: 'Саша Печкин',
-		text: 'В четверг, четвертого числа'
+		text: 'В четверг, четвертого числа...',
+		bigText: 'в четыре с четвертью часа четыре черненьких чумазеньких чертенка чертили черными чернилами чертеж.'
 	},
 	{
 		author: 'Просто Вася',
-		text: 'Бла бла'
+		text: 'Бла бла',
+		bigText: 'Пвек шмек теребек'
 	},
 	{
 		author: 'Саша печкин',
-		text: 'В четверг, четвертого числа'
+		text: 'В четверг, четвертого числа',
+		bigText: 'я повторяюсь раза два'
 	}
 ];
 
@@ -17,16 +20,33 @@ var Article = React.createClass({
 	propTypes: {
 		data: React.PropTypes.shape({
 			author: React.PropTypes.string.isRequired,
-			text: React.PropTypes.string.isRequired
+			text: React.PropTypes.string.isRequired,
+			bigText: React.PropTypes.string.isRequired
 		})
+	},
+	getInitialState: function() {
+		return {
+			visible: false
+		};
+	},
+	readmoreClick: function(event) {
+		event.preventDefault();
+		this.setState({visible: true});
 	},
 	render: function() {
 		var data = this.props.data;
+		var state = this.state;
 
 		return (
 			<div className="article">
 				<p className="news__author">{data.author}</p>
 				<p className="news__text">{data.text}</p>
+				<a href="#"
+				   className={'news__readmore ' + (state.visible ? 'none' : '')}
+				   onClick={this.readmoreClick}>
+					Подробнее
+				</a>
+				<p className={'news__big-text ' + (!state.visible ? 'none' : '')}>{data.bigText}</p>
 			</div>
 		);
 	}
@@ -55,8 +75,34 @@ var News = React.createClass({
 		return (
 			<div className="news">
 				{newsTemplate}
-				<strong className={'news__count ' + (data.length > 0 ? '' : 'none')}>Всего новостей {data.length}</strong>
+				<strong className={'news__count ' + (data.length > 0 ? '' : 'none')}>
+					Всего новостей {data.length}
+				</strong>
 			</div>
+		);
+	}
+});
+
+var TestInput = React.createClass({
+	getInitialState: function() {
+		return {
+			value: ''
+		};
+	},
+	onChange: function(event) {
+		this.setState({
+			value: event.target.value
+		})
+	},
+	render: function() {
+		var state = this.state;
+
+		return (
+			<input type="text"
+			       className="test-input"
+			       value={state.value}
+			       onChange={this.onChange}
+			       placeholder="Введите значение"/>
 		);
 	}
 });
@@ -66,6 +112,7 @@ var App = React.createClass({
 		return (
 			<div className="app">
 				<h3>Новости</h3>
+				<TestInput />
 				<News data={myNews}/>
 			</div>
 		);
